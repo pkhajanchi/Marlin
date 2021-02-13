58c8c20e0282b995ddde32b236c97ca8a729d456
Pankaj Khajanchi <pankaj.khajanchi@gmail.com>
2021-02-13 20:47:45 +0530

    build plate changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1167,7 +1167,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -15
 #define Z_MIN_POS -2
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
