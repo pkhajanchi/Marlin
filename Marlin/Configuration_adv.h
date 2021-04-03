7860633eb7d3c600493b99cdafc17e4f7de9b915
Pankaj Khajanchi <pankaj.khajanchi@gmail.com>
2021-04-03 12:38:25 +0530

    Decreased Z Current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2381,7 +2381,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       580
+    #define Z_CURRENT       280
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2417,7 +2417,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      300
+    #define E0_CURRENT      280
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
@@ -2425,7 +2425,7 @@
   #endif
 
   #if AXIS_IS_TMC(E1)
-    #define E1_CURRENT      300
+    #define E1_CURRENT      280
     #define E1_MICROSTEPS    16
     #define E1_RSENSE         0.11
     #define E1_CHAIN_POS     -1
